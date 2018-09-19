40762b70fbd72ee6e39813426a3798115c1242d5
Đoàn VIệt Anh <hongnhungdoan161@gmail.com>
2018-09-19 17:50:41 +0700

    baitap

---
+++ git_ss4_hw.py
@@ -0,0 +1,29 @@
+1.
+- A Boolean value is either true or false.
+- 3 differrent expression that results a Boolean type:
++ 5 == (1+4)
+=>	True
++ name = “viet anh”
+   “hoc sinh ” + name ==  “hoc sinh viet anh”
+=>	True
++ x=5
+  y=6
+  z=7
+(x+y) <= z
+=>	False
+2.
+- Flow chart is a type of diagram that represent an algorithm, workflow or process. It shows steps as box of various kind	and their order is connected with arrows. A flow chart represents its solution model to the given problem.
+
+-Image for the flowchart:
+ https://scontent.fhan1-1.fna.fbcdn.net/v/t1.15752-0/p280x280/42084533_2164659937107295_7853463557478809600_n.jpg?_nc_cat=104&oh=b662becbd9612aa5ae3dd6bceb78108e&oe=5C36BCB1
+
+3.
+- Nested is one conditional being nested within another.
+- if x==y:
+ print (“x=y”)
+else:
+
+if x<y:
+print (“x<y”)
+else:
+print (“x>y”)
