a5eecc1115c93a344b07fe46fee9863d3dc87bb1
Đoàn VIệt Anh <hongnhungdoan161@gmail.com>
2018-09-22 18:05:39 +0700

    sua lai bai

--- git_guess_my_number.py
+++ git_guess_my_number.py
@@ -3,19 +3,21 @@ print ("chon so tu nhien tu 0 den 100")
 guest_number = int(input("nhap so cua ban: ")) 
 i=0
 loop = True
+computer_number= randint(0,100)
 while loop:
-    computer_number = randint(0,100)
     i+=1
     if computer_number == guest_number:
         print ("may tinh doan la: ",computer_number) 
         print ("may da doan dung")
         loop = False
     elif computer_number < guest_number:
-        print ("may tinh doan la: ",computer_number) 
-        print ("so ban lon hon")
+        print ("may tinh doan la: ",computer_number)
+        print ("so ban lon hon") 
+        computer_number = randint(computer_number+1,100)
     else:
         print ("may tinh doan la: ",computer_number) 
         print ("so ban be hon ")
+        computer_number = randint (0,computer_number)
     if i>8:
         print("may tinh da het luot doan\nGame over\nban thang")
         loop = False
